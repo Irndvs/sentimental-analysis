b1c706dc3436f94cfe3e06e07f7eba9c86d63df2
Irndvs <162671823+Irndvs@users.noreply.github.com>
2024-05-01 21:39:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import joblib
-model =joblib.load("sentiment-model.pkl")
-sentiment_labels= {'1': 'Positive', '0': 'Negative'}
+
+model=joblib.load("sentiment-model.pkl")
+
+sentiment_labels={"1": "Positive", "0": "Negative"}
 
 st.title("Sentiment Analysis")
-user_input =st.text_area("Enter your text here:")
+
+user_input= st.text_area("Enter the you text here:")
+
+
 if st.button("Predict"):
     print(user_input)
-    predicted_sentiment= model.predict([user_input])[0]
+    predicted_sentiment=model.predict([user_input])[0]
     print("Predicted Label:" + str(predicted_sentiment))
-    predicted_sentiment_label = sentiment_labels[str(predicted_sentiment)]
-    st.info(f"Predicted Sentiment:{predicted_sentiment_label}")
+    predicted_sentiment_label=sentiment_labels[str(predicted_sentiment)]
+
+    st.info(f"Predicted Sentiment: {predicted_sentiment_label}")
